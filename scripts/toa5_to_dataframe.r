2b85115bc3c8010948a79052feff6c60b69dfd03
Nithiya Streethran <nmstreethran@gmail.com>
2023-12-24 17:47:49 +0000

    delete old scripts

--- scripts/toa5_to_dataframe.r
+++
@@ -1,7 +0,0 @@
-install.packages("readmet")
-
-library("readmet")
-
-dat <- read.toa5("data/fluxes_201415/TOA5_2806.ts_data.dat")
-dat$time <- as.POSIXct(dat$TIMESTAMP)
-write.csv(dat, "data/fluxes_201415/TOA5_2806.ts_data.dat.csv")
