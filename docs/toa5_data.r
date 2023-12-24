2a4d486f9a369b1128c6e0f2b1f9563d74ce52d7
Nithiya Streethran <nmstreethran@gmail.com>
2023-12-24 19:20:47 +0000

    format script

--- docs/toa5_data.r
+++ docs/toa5_data.r
@@ -1,46 +1,33 @@
+# split large (~28 mil rows, ~3 GB) TOA5 file into manageable
+# (max. 1 mil rows, ~110 MB) files
+
+# read TOA5 dataset
 data <- read.csv("TOA5_2806.ts_data.dat", skip = 4, header = FALSE)
 
+# assign column names
 names(data) <- as.matrix(
-    read.csv("TOA5_2806.ts_data.dat", skip = 1, header = FALSE, nrows = 1)
+  read.csv("TOA5_2806.ts_data.dat", skip = 1, header = FALSE, nrows = 1)
 )
 
-head(data)
-
+# extract headers
 headers <- read.csv(
-    "TOA5_2806.ts_data.dat", skip = 1, header = FALSE, nrows = 3
+  "TOA5_2806.ts_data.dat", skip = 1, header = FALSE, nrows = 3
 )
 
-headers
-
+# save headers
 write.table(
-    headers, "TOA5_2806.headers.csv", row.names = FALSE, col.names = FALSE,
-    sep = ",", quote = FALSE
+  headers, "TOA5_2806.headers.csv", row.names = FALSE, col.names = FALSE,
+  sep = ",", quote = FALSE
 )
 
-summary(data)
-
-nrow(data)
-
+# split data into manageable chunks (1 mil rows)
 data_split <- split(data, (as.numeric(rownames(data)) - 1) %/% 1000000)
 
-str(data_split)
-
-str(data_split[[1]])
-
-str(data_split[[29]])
-
-head(data_split[[1]])
-
-head(data_split[[29]])
-
-nrow(data_split[[1]])
-
-nrow(data_split[[29]])
-
+# save each chunk as a new CSV file
 for (n in 1:length(data_split)) {
-    write.table(
-        data_split[[n]],
-        paste("TOA5_2806.", "ts_data.", sprintf("%02d", n), ".csv", sep = ""),
-        row.names = FALSE, sep = ",", quote = FALSE
-    )
+  write.table(
+    data_split[[n]],
+    paste("TOA5_2806.", "ts_data.", sprintf("%02d", n), ".csv", sep = ""),
+    row.names = FALSE, sep = ",", quote = FALSE
+  )
 }
